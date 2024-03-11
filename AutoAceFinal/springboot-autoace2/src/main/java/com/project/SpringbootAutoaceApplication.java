ead91b58a19ab8035a4a327b8f194b94888d4d16
Vedantika <pvedu2001@gmail.com>
2024-03-12 00:13:21 +0530

    final

---
+++ AutoAceFinal/springboot-autoace2/src/main/java/com/project/SpringbootAutoaceApplication.java
@@ -0,0 +1,12 @@
+package com.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAutoaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAutoaceApplication.class, args);
+	}
+}
